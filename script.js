5d9dbe27a99cbc62f9ce0919e3f0a3b56f9d9531
almeidadevweb <almeidadevweb@gmail.com>
2026-05-18 23:00:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const form = document.getElementById('contactForm')
-const successMessage = document.getElementById('successMessage')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  successMessage.innerText = 'Mensagem enviada com sucesso!'
-
-  form.reset()
-
-  setTimeout(() => {
-    successMessage.innerText = ''
-  }, 4000)
-})
